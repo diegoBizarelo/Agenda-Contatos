8a195f553c84ab80947adc310ed66d1fbf5de995
Diego Bizarelo <diego.bizarelo@gmail.com>
2020-09-24 17:33:08 -0300

    Implementado Interface de repositorio no dominio e classe repositorio na infrastrutura

---
+++ src/SGA.ApplicationCore/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGA.ApplicationCore.Interfaces.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Add(T entity);
+        T UpDate(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        IEnumerable<T> Search(Expression<Func<T, bool>> predicate);
+        void Remove(T entity);
+    }
+}
---
+++ src/SGA.Infrastructure/Repository/EFRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGA.ApplicationCore.Interfaces.Repository;
+using SGA.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGA.Infrastructure.Repository
+{
+    public class EFRepository<T> : IRepository<T> where T : class
+    {
+
+        private readonly PessoaContext _dbContext;
+        public EFRepository(PessoaContext context)
+        {
+            _dbContext = context;
+        }
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> Search(Expression<Func<T, bool>> predicate)
+        {
+            return _dbContext.Set<T>().Where(predicate).AsEnumerable();
+        }
+
+        public T UpDate(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return entity;
+        }
+    }
+}
